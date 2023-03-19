2015ce4aba3c3c4d7e0d308f9117ce5673411408
Erick Hernandez <yerickk8@gmail.com>
2023-03-19 10:38:06 -0600

    setup project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path = require("path")
+const hbs = require('hbs')
+
+const templatePath = path.join(__dirname, '../tempelates')
+
+app.use(express.json())
+app.set('view engine', 'hbs')
+app.set('views', templatePath)
+
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
