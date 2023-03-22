c213bb9ccc62ab1c67cf82f6c2148127683d4e26
Erick Hernandez <yerickk8@gmail.com>
2023-03-22 11:20:30 -0600

    Connected Mongo DB

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require("path")
 const hbs = require('hbs')
+const collection = require('./mongo')
 
 const templatePath = path.join(__dirname, '../tempelates')
 
@@ -17,6 +18,21 @@ app.get('/', (req, res) => {
 app.get('/signup', (req, res) => {
     res.render('signup');
 });
+
+app.post('/signup', async (req, res) => {
+
+    const data = {
+        name: req.body.name,
+        password: req.body.password,
+
+    }
+
+    await collection.insertMany([data])
+
+    res.render("home")
+
+
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
