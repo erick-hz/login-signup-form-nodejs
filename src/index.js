0c44a79eb19ec69bb655703bfd2fad2e5cba9ffd
Erick Hernandez <yerickk8@gmail.com>
2023-03-24 10:46:33 -0600

    Added validations

--- src/index.js
+++ src/index.js
@@ -1,39 +1,99 @@
-const express = require('express')
-const app = express()
+const express = require("express")
 const path = require("path")
-const hbs = require('hbs')
-const collection = require('./mongo')
-
-const templatePath = path.join(__dirname, '../tempelates')
-
+const app = express()
+// const hbs = require("hbs")
+const LogInCollection = require("./mongo")
+const port = process.env.PORT || 3000
 app.use(express.json())
+
+app.use(express.urlencoded({ extended: false }))
+
+const tempelatePath = path.join(__dirname, '../tempelates')
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath);
+
 app.set('view engine', 'hbs')
-app.set('views', templatePath)
+app.set('views', tempelatePath)
+app.use(express.static(publicPath))
 
 
-app.get('/', (req, res) => {
-    res.render('login');
-});
+// hbs.registerPartials(partialPath)
+
 
 app.get('/signup', (req, res) => {
-    res.render('signup');
-});
+    res.render('signup')
+})
+app.get('/', (req, res) => {
+    res.render('login')
+})
+
+
+
+// app.get('/home', (req, res) => {
+//     res.render('home')
+// })
 
 app.post('/signup', async (req, res) => {
 
+    // const data = new LogInCollection({
+    //     name: req.body.name,
+    //     password: req.body.password
+    // })
+    // await data.save()
+
     const data = {
         name: req.body.name,
-        password: req.body.password,
+        password: req.body.password
+    }
+
+    const checking = await LogInCollection.findOne({ name: req.body.name })
+
+    try {
+        if (checking.name === req.body.name && checking.password === req.body.password) {
+            res.send("user details already exists")
+        }
+        else {
+            await LogInCollection.insertMany([data])
+        }
+    }
+    catch {
+        res.send("wrong inputs")
+    }
+
+    res.status(201).render("home", {
+        naming: req.body.name
+    })
+})
+
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const check = await LogInCollection.findOne({ name: req.body.name })
+
+        if (check.password === req.body.password) {
+            res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` })
+        }
+
+        else {
+            res.send("incorrect password")
+        }
+
 
     }
 
-    await collection.insertMany([data])
+    catch (e) {
 
-    res.render("home")
+        res.send("wrong details")
+
+
+    }
 
 
 })
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+
+
+app.listen(port, () => {
+    console.log('port connected');
+})
